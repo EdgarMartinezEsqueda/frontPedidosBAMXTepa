825954e8393073c8024b2b74aac233e6a9cbdd9e
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-21 13:14:33 -0600

    feat: add home page

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "components/navbar/Navbar";
+import Footer from "components/footer/Footer";
+
+const HomePage = () => {
+  return (
+      <>
+      <Navbar />
+      <Footer />
+      </>
+  );
+};
+
+export default HomePage;
