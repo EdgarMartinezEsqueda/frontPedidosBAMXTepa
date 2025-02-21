e679a9d12e195bf52e2f3e02b05fd891e72021d6
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-02-21 13:21:59 -0600

    refactor: update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,7 +13,7 @@ export default defineConfig({
     alias: {
       components: path.resolve(__dirname, "./src/components"),
       assets: path.resolve(__dirname, "./src/assets"),
-      fonts: path.resolve(__dirname, "./public/fonts"),
+      fonts: path.resolve(__dirname, "./fonts"),
     }
   }
 })
