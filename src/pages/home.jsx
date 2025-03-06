8c611edf9da4c1d8c191bdded308d500cd68a2b8
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-03-06 10:17:29 -0600

    feat: update home page
    Now show the orders table

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,16 @@
 import Navbar from "components/navbar/Navbar";
 import Footer from "components/footer/Footer";
+import TableRoutes from "components/tables/TableRoutes";
 
 const HomePage = () => {
   return (
-      <>
+      <div className="min-h-screen flex flex-col">
       <Navbar />
+      <main className="flex-1">
+        <TableRoutes />
+      </main>
       <Footer />
-      </>
+      </div>
   );
 };
 
