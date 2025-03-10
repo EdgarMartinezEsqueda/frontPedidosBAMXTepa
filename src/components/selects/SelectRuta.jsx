d99a89531271a1e4fef0ba50a4f4906ef1e106d1
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-03-10 10:58:04 -0600

    feat: Add route's selector

---
+++ src/components/selects/SelectRuta.jsx
@@ -0,0 +1,23 @@
+const SelectRuta = ({ rutas = [], value, onChange }) => {
+  return (
+    <>
+      <select
+        name="HeadlineAct"
+        id="HeadlineAct"
+        className="border-gray-300 text-amarilloLogo"
+        onChange={onChange}
+        value={value}
+      >
+        <option value="">Elige la ruta</option>
+        { rutas 
+          ? rutas.map((item, index) => (
+            <option key={index} value={item.nombre}>
+              {item.nombre}
+            </option>
+          ) ) : (null)}
+      </select>
+    </>
+  );
+}
+
+export default SelectRuta;
