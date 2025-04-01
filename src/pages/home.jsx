f9f52bf00137cc543e187c5fff73c12722f40186
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-04-01 10:42:18 -0600

    feat: add filters on the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import Navbar from "components/navbar/Navbar";
 import Footer from "components/footer/Footer";
 import TableRoutes from "components/tables/TableRoutes";
+import FilterOrders from "components/filter/FilterOrders";
 
 const HomePage = () => {
   return (
       <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">
+        <FilterOrders />
         <TableRoutes />
       </main>
       <Footer />
