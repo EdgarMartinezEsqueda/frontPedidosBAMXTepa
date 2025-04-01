b13585b745d150e59499e8faf68ede0c256d8b00
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-04-01 10:43:47 -0600

    feat: add backend alias

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,35 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      components: path.resolve(__dirname, "./src/components"),
-      assets: path.resolve(__dirname, "./src/assets"),
-      fonts: path.resolve(__dirname, "./fonts"),
-    }
+export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        assets: path.resolve(__dirname, "./src/assets"),
+        components: path.resolve(__dirname, "./src/components"),
+        context: path.resolve(__dirname, "./src/context"),
+        fonts: path.resolve(__dirname, "./fonts"),
+        hooks: path.resolve(__dirname, "./src/hooks"),
+        lib: path.resolve(__dirname, "./src/lib"),
+      }
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      }
+    },
   }
 })
