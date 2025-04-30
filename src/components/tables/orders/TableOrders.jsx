bab54348ee9779b4bfa4223bd2a67e6d9cc1bc2c
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-04-30 00:07:32 -0600

    feat: add orders table component

---
+++ src/components/tables/orders/TableOrders.jsx
@@ -0,0 +1,184 @@
+import React, { useMemo } from "react";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
+
+import api from "lib/axios";
+import { useAuth } from "context/AuthContext";
+
+import { hasPermission, RESOURCES } from "utils/permisos";
+import { FaRegEdit, FaRegEye } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+
+const TableComponent = ({  currentPage, pageSize, filters, setTotalOrders }) => {
+  const { user } = useAuth();
+  
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  // Obtiene TODOS los datos paginados del servidor
+  const { data: allOrders, isLoading } = useQuery({
+    queryKey: ["pedidos", currentPage, pageSize],
+    queryFn: async () => {
+      const { data } = await api.get("/pedidos", {
+        params: { page: currentPage, pageSize }
+      });
+      setTotalOrders(data.length);
+      return data;
+    }
+  });
+
+  // Filtrar datos localmente
+  const filteredData = useMemo(() => {
+    if (!allOrders) return [];
+    
+    return allOrders.filter(order => {
+      const matchesUser = filters.usuarios.length === 0 ||  filters.usuarios.includes(order.usuario.username);
+      const matchesRoute = filters.rutas.length === 0 ||  filters.rutas.includes(order.ruta.nombre);
+      const matchesMunicipality = filters.estatusPedido.length === 0 ||  filters.estatusPedido.includes(order.estado);
+      
+      const { startDate, endDate } = filters.rangoFechas || {};
+      const orderDate = order.fechaEntrega;  // formato "YYYY-MM-DD"
+      const matchesDate = (!startDate || orderDate >= startDate) &&  (!endDate || orderDate <= endDate);
+    
+      return matchesUser && matchesRoute && matchesMunicipality && matchesDate;
+    });
+  }, [allOrders, filters]);
+
+  // Paginación local
+  const paginatedData = useMemo(() => {
+    const start = (currentPage - 1) * pageSize;
+    return filteredData.slice(start, start + pageSize);
+  }, [filteredData, currentPage, pageSize]);
+
+  
+  const deleteMutation = useMutation({
+    mutationFn: (id) => api.delete(`/pedidos/${id}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["pedidos"]);
+      toast.success("Pedido eliminado correctamente");
+    },
+    onError: () => {
+      toast.error("Error eliminando el pedido");
+    }
+  });
+  
+  return (
+    <section className="container mx-auto m-6">
+      <div className="w-full overflow-hidden rounded-lg lg:border lg:border-gray-200 lg:dark:border-gray-700">
+        <table className="w-full divide-y divide-gray-200 dark:divide-gray-700 text-black dark:text-white">
+          {/* Cabecera solo para desktop */}
+          <thead className="hidden lg:table-header-group bg-gray-50 dark:bg-gray-800">
+            <tr>
+              {["ID", "Ruta", "Fecha", "Estatus", "Trabajador social", "Opciones"].map((header) => (
+                <th key={header} className="px-4 py-3.5 text-sm font-medium text-gray-900 dark:text-white text-center"> {header}</th>
+              ))}
+            </tr>
+          </thead>
+          
+          <tbody className="divide-y lg:divide-gray-200 lg:dark:divide-gray-700 lg:dark:bg-gray-900">
+            {paginatedData.length === 0 ? (
+              <tr className="block lg:table-row dark:bg-gray-800 rounded-lg shadow p-4 mb-4 lg:p-0 lg:shadow-none lg:bg-transparent">
+                <td 
+                  colSpan="6" 
+                  className="px-4 py-4 text-sm text-center text-gray-500 dark:text-gray-400 block lg:table-cell"
+                >
+                  No hay datos disponibles.
+                </td>
+              </tr>
+            ) : (
+              paginatedData.map((item, index) => (
+                <tr 
+                  key={index}
+                  className="block lg:table-row dark:bg-gray-800 rounded-lg shadow p-4 mb-4 lg:p-0 lg:shadow-none lg:bg-transparent"
+                >
+                  {/* ID */}
+                  <td data-th="ID" className="hidden lg:table-cell px-4 py-4 text-sm whitespace-nowrap lg:!bg-inherit bg-gray-50 dark:bg-gray-800 text-center" >
+                    <div className="flex justify-between items-center lg:block gap-2">
+                      <span className="text-gray-400 lg:hidden">ID</span>
+                      <span className="dark:text-white">{item.id}</span>
+                    </div>
+                  </td>
+
+                  {/* Ruta */}
+                  <td data-th="Ruta" className="block lg:table-cell px-4 py-4 text-sm whitespace-nowrap lg:!bg-inherit bg-gray-50 dark:bg-gray-800 text-center" >
+                    <div className="flex justify-between items-center lg:block gap-2">
+                      <span className="text-gray-400 lg:hidden">Ruta</span>
+                      <span className="dark:text-white">{item.ruta.nombre}</span>
+                    </div>
+                  </td>
+
+                  {/* Fecha */}
+                  <td data-th="Fecha" className="block lg:table-cell px-4 py-4 text-sm whitespace-nowrap lg:!bg-inherit bg-gray-50 dark:bg-gray-800 text-center" >
+                    <div className="flex justify-between items-center lg:block gap-2">
+                      <span className="text-gray-400 lg:hidden">Fecha</span>
+                      <span className="dark:text-white">{item.fechaEntrega}</span>
+                    </div>
+                  </td>
+
+                  {/* Estatus */}
+                  <td data-th="Estatus" className="block lg:table-cell px-4 py-4 text-sm whitespace-nowrap lg:!bg-inherit bg-gray-50 dark:bg-gray-800 text-center" >
+                    <div className="flex justify-between items-center lg:block gap-2">
+                      <span className="text-gray-400 lg:hidden">Estatus</span>
+                      <span className={`px-2 py-1 rounded-full dark:text-white`}>
+                        {item.estado}
+                      </span>
+                    </div>
+                  </td>
+
+                  {/* Trabajador */}
+                  <td data-th="Trabajador" className="block lg:table-cell px-4 py-4 text-sm whitespace-nowrap lg:!bg-inherit bg-gray-50 dark:bg-gray-800 text-center" >
+                    <div className="flex justify-between items-center lg:block gap-2">
+                      <span className="text-gray-400 lg:hidden">Trabajador</span>
+                      <span className="dark:text-white">{item.usuario.username}</span>
+                    </div>
+                  </td>
+
+                  {/* Opciones */}
+                  <td className="block lg:table-cell px-4 py-4 text-sm whitespace-nowrap lg:!bg-inherit bg-gray-50 dark:bg-gray-800 text-center" >
+                    <div className="flex justify-between lg:justify-center items-center gap-x-6 text-lg">
+                      <span className="text-gray-400 lg:hidden">Opciones</span>
+                      <div className="flex gap-x-6">
+                        <button
+                          className="text-gray-500 transition-colors duration-200 dark:hover:text-green-500 dark:text-gray-300 hover:text-green-500 focus:outline-none cursor-pointer"
+                          onClick={() => navigate(`/pedido/${item.id}`)}
+                        >
+                          <FaRegEye />
+                        </button>
+                        
+                        {hasPermission(user.data, RESOURCES.PEDIDOS, "update", item.idTs ) && (
+                          <button
+                            className={`text-gray-500 transition-colors duration-200 dark:text-gray-300 focus:outline-none ${
+                              item.estado === "finalizado" 
+                                ? "cursor-not-allowed" 
+                                : "cursor-pointer hover:text-yellow-500"
+                            }`}
+                            onClick={() => navigate(`/pedido/editar/${item.id}`)}
+                            disabled={item.estado === "finalizado"}
+                          >
+                            <FaRegEdit />
+                          </button>
+                        )}
+
+                        {hasPermission(user.data, RESOURCES.PEDIDOS, "delete", item.idTs ) && (
+                          <button
+                            className="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none cursor-pointer"
+                            onClick={() => deleteMutation.mutate(item.id)}
+                          >
+                            <MdDeleteOutline />
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default TableComponent;
