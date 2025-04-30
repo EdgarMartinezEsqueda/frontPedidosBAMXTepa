8439fa7b1dbcdfb65da994360352be3f080e358d
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-04-30 00:23:17 -0600

    feat: add edit order page

--- src/pages/pedido/editarPedido.jsx
+++ src/pages/pedido/editarPedido.jsx
@@ -1,42 +1,185 @@
-import { useState } from "react";
-import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { useAuth } from "context/AuthContext";
+import toast from "react-hot-toast";
+import api from "lib/axios";
+import { hasPermission, RESOURCES } from "utils/permisos";
 
 import Navbar from "components/navbar/Navbar";
 import Footer from "components/footer/Footer";
 import TableOrder from "components/tables/orders/TableOrder";
 import GroupButtons from "components/buttons/ButtonsForOrderEdit";
 
-const HomePage = ( props ) => {
+const EditOrder = ( ) => {
   const { id } = useParams();
-  const disabled = props || props.lenght == 0;
-  const initialData = "";   // AQUI ESTARÁ LA DATA DEL PEDIDO
-  const [editableData, setEditableData] = useState(initialData);
+  const navigate = useNavigate();
+  const { user } = useAuth();
 
+  //Fetch para obtener los datos del pedido
+  const { data: pedido, isLoading: isLoadingPedido, isError: isErrorPedido, error: errorPedido } = useQuery({
+    queryKey: ["pedido"],
+    queryFn: async () => {
+      const response = await api.get(`/pedidos/${id}`);
+      return response.data;
+    },
+  });
+
+  // Estado editable
+  const [editableData, setEditableData] = useState({
+    fechaEntrega: "",
+    estado: "pendiente",
+    devoluciones: 0,
+    pedidoComunidad: [],
+  });
+  
+  useEffect(() => {
+    if (pedido) {
+      setEditableData({
+        fechaEntrega: pedido.fechaEntrega || "",
+        estado: pedido.estado || "pendiente",
+        devoluciones: pedido.devoluciones ?? 0, // Previene null
+        idRuta: pedido.idRuta,
+        pedidoComunidad: pedido.pedidoComunidad.map((comunidad) => ({
+          ...comunidad,
+          despensasCosto: comunidad.despensasCosto ?? 0,
+          despensasMedioCosto: comunidad.despensasMedioCosto ?? 0,
+          despensasSinCosto: comunidad.despensasSinCosto ?? 0,
+          despensasApadrinadas: comunidad.despensasApadrinadas ?? 0,
+          observaciones: comunidad.observaciones ?? ""
+        })),
+      });
+    }
+  }, [pedido]);  
+
+  // Mutaciones
+  const updateMutation = useMutation({
+    mutationFn: (data) => {
+      return api.patch(`/pedidos/${id}`, {
+      fechaEntrega: data.fechaEntrega,
+      pedidoComunidad: data.pedidoComunidad,
+      devoluciones: data.devoluciones
+    })},
+    retry:0,
+    onSuccess: (res) => {
+      toast.success("Cambios guardados");
+      navigate(`/pedido/${id}`);
+    },
+    onError: (error) => toast.error(error.response?.data?.error.message || "Error guardando") ,
+  });
+
+  const finalizeMutation = useMutation({
+    mutationFn: (devoluciones) => api.patch(`/pedidos/${id}`, {
+      estado: "finalizado",
+      devoluciones: devoluciones,
+    }),
+    onSuccess: () => {
+      toast.success("Pedido finalizado");
+      setEditableData(prev => ({ ...prev, estado: "finalizado" }));
+      navigate(`/pedido/${id}`);
+    },
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: () => api.delete(`/pedidos/${id}`),
+    onSuccess: () => {
+      toast.success("Pedido eliminado");
+      navigate(`/`);
+    },
+  });
+
+  // Verifica permisos del usuario
+  useEffect(() => {
+    if (pedido && user.data) {
+
+      if(pedido.estado === "finalizado") navigate(`/pedido/${id}`); // Evita editar pedidos finalizados
+
+      const isAdmin = user.data.rol === 'Direccion';
+      const isOwner = pedido.idTs === user.data.id;
+      
+      if (!isAdmin && !isOwner) {
+        navigate('/404', { replace: true });
+      }
+    }
+  }, [pedido, user.data, navigate]);
+
+  // Handlers
+  const handleSave = () => updateMutation.mutate(editableData);
+  const handleFinalize = () => {
+    if (editableData.devoluciones < 0) {
+      toast.error("Las despensas regresadas no pueden ser negativas");
+      return;
+    }
+    finalizeMutation.mutate(editableData.devoluciones);
+    navigate(-1);
+  };
+
+  const handleDelete = () => {
+    if (window.confirm("¿Borrar pedido permanentemente?")) {
+      deleteMutation.mutate();
+    }
+  };
+
+  const isProcessing = updateMutation.isPending || finalizeMutation.isPending || deleteMutation.isPending;
+  const disabled = isLoadingPedido || isProcessing;
+
+  
+  if (isLoadingPedido) return <div>Cargando...</div>;
+  if (isErrorPedido) return <div>Error: {errorPedido.message}</div>;
+  
   return (
       <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">
-        <h2 className="font-bold text-2xl w-full text-center pt-4 text-verdeLogo"> Editar pedido Ruta { id }</h2>
+        <h2 className="font-bold text-2xl w-full text-center pt-4 text-verdeLogo"> Editar pedido #{ id }</h2>
         
+        {/* Campos principales */}
+        <div className="max-w-md mx-auto my-2 space-y-4 p-4">
+
+          <div className="flex flex-col gap-2">
+            <label>Fecha de entrega</label>
+            <input
+              type="date"
+              value={editableData.fechaEntrega}
+              onChange={(e) => setEditableData({ ...editableData, fechaEntrega: e.target.value })}
+              className="p-2 border rounded"
+              disabled={disabled}
+            />
+          </div>
+        </div>
+
         <TableOrder
           mode="edit"
           data={editableData}
-          onDataChange={setEditableData}
+          onDataChange={(newComunidades) =>
+            setEditableData({ ...newComunidades })
+          }
         />
         
-        <div className="flex justify-center items-center flex-col max-w-md m-auto">
-          <h2 htmlFor="regresadas" className="block font-bold text-2xl text-rojoLogo">Despensas regresadas</h2>
-          <div className="relative flex items-center mt-2">
-              <input type="text" placeholder="0" className={`block w-full py-2.5 text-gray-700 placeholder-gray-400/70 bg-white border border-gray-200 rounded-lg pl-11 pr-5 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 
-              ${ disabled
-                ? "text-gray-400 cursor-not-allowed dark:text-gray-500"
-                : "focus:border-amarilloLogo dark:focus:border-amarilloLogo focus:ring-amarilloLogo focus:outline-none focus:ring focus:ring-opacity-40"
-              }`} />
+        {/* Despensas regresadas y estado */}
+        <div className="max-w-md mx-auto my-2 space-y-4 ">
+          <div className="flex flex-col gap-2">
+            <label className="text-rojoLogo font-bold">Despensas regresadas</label>
+            <input
+              type="number"
+              min="0"
+              value={editableData.devoluciones}
+              onChange={(e) => setEditableData({ ...editableData, devoluciones: Math.max(0, e.target.valueAsNumber) })}
+              className="p-2 border rounded"
+              disabled={disabled || editableData.estado === "finalizado"}
+            />
           </div>
-        </div>
         
-        <div className="flex justify-center py-6">
-          <GroupButtons disabled={disabled} />
+          {hasPermission(user.data, RESOURCES.PEDIDOS, "update", pedido.idTs) &&
+            <div className="flex items-center justify-center w-full">
+              <GroupButtons
+                disabled={disabled}
+                onSave={handleSave}
+                onFinalize={editableData.estado !== "finalizado" ? handleFinalize : null}
+                onDelete={handleDelete}
+                />
+            </div>
+          }
         </div>
       </main>
       <Footer />
@@ -44,4 +187,4 @@ const HomePage = ( props ) => {
   );
 };
 
-export default HomePage;
+export default EditOrder;
