1b6800ca89166d14be8291809fb5ecfc9cbff45f
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-04-29 23:36:55 -0600

    style: improve 404 page to full-screen layout

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,11 +4,11 @@ import Footer from "components/footer/Footer";
 
 const ErrorPage = () =>{
     return (
-      <>
+      <div className="min-h-screen flex flex-col justify-between">
         <Navbar/>
         <Error404/>
         <Footer/>
-      </>
+      </div>
     );
 };
 
