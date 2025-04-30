68dbdaec3668d8268d278b2334ddb5f3b99ee292
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-04-29 23:34:26 -0600

    feat: improve 404 page to full-screen layout

--- src/components/error/404.jsx
+++ src/components/error/404.jsx
@@ -1,16 +1,20 @@
 import img404 from "assets/404.svg";
 
 const Error404 = () => {
-    return (
-      <section className="bg-white dark:bg-bgOscuro1 p-10">
-        <div className="flex flex-col items-center justify-center ">
-          <img src={img404} className="h-96 fill-blue"/>
-          <h1 className="text-4xl text-center font-omnes font-bold p-12">
-            Página no encontrada
-          </h1>
-        </div>
-      </section>
-    );
+  return (
+    <section className="flex-1 bg-white dark:bg-bgOscuro1">
+      <div className="container mx-auto h-full min-h-[calc(100vh-150px)] flex flex-col items-center justify-center px-4 md:px-10 py-8">
+        <img 
+          src={img404} 
+          className="max-h-[50vh] w-full md:max-w-2xl object-contain"
+          alt="Error 404" 
+        />
+        <h2 className="text-3xl md:text-4xl text-center font-omnes font-bold mt-6 md:mt-12 px-4 dark: text-white">
+          Página no encontrada
+        </h2>
+      </div>
+    </section>
+  );
 }
 
 export default Error404;
