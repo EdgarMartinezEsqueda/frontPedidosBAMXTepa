00a50722d60a21212fc01fdc3c0eea719da52e12
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-05-05 00:46:28 -0600

    feat: add forgot and reset password pages

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ import EditCommunity from "./pages/comunidad/editarComunidad";
 import Users from "./pages/admin/usuarios/usuarios";
 import NewUser from "./pages/admin/usuarios/nuevoUsuario";
 import EditUser from "./pages/admin/usuarios/editarUsuario";
-import ReportesResumen from "./pages/admin/reportes/resumen.jsx"
-import ReportesRutas from "./pages/admin/reportes/rutas.jsx"
-import ReportesTS from "./pages/admin/reportes/ts.jsx"
-import ReportesDespensas from "./pages/admin/reportes/despensas.jsx"
-import ReportesComunidades from "./pages/admin/reportes/comunidades.jsx"
-import ReportesApadrinadas from "./pages/admin/reportes/apadrinadas.jsx"
+import ReportesResumen from "./pages/admin/reportes/resumen.jsx";
+import ReportesRutas from "./pages/admin/reportes/rutas.jsx";
+import ReportesTS from "./pages/admin/reportes/ts.jsx";
+import ReportesDespensas from "./pages/admin/reportes/despensas.jsx";
+import ReportesComunidades from "./pages/admin/reportes/comunidades.jsx";
+import ReportesApadrinadas from "./pages/admin/reportes/apadrinadas.jsx";
+import ForgotPassword from "./pages/auth/forgotPassword.jsx";
+import ResetPassword from "./pages/auth/resetPassword.jsx";
 
 import { useAuth } from "context/AuthContext";
 import { ThemeProvider } from "context/ThemeContext";
@@ -65,6 +67,9 @@ function App() {
     { path: "/rutas/editar/:id", element: <EditRoute />, requiresAuth: true, title: "Editar ruta", allowedRoles: ["Direccion", "Coordinadora"] },
     { path: "/rutas/:id", element: <Ruta />, requiresAuth: true, title: "Ruta", allowedRoles: ["Direccion", "Ts", "Coordinadora"] },
 
+    { path: "/forgotPassword", element: <ForgotPassword />, requiresAuth: false, title: "Recuperar contraseña"},
+    { path: "/resetPassword/:token", element: <ResetPassword />, requiresAuth: false, title: "Cambiar contraseña"},
+
     { path: "*", element: <ErrorPage />, requiresAuth: true, title: "Ha surgido un error", allowedRoles: ["Direccion", "Ts", "Almacen", "Coordinadora"] },
   ];
 
@@ -73,13 +78,17 @@ function App() {
   }
 
   const checkAccess = (route, user) => {
-    if (route.requiresAuth && !user) return false;
-  
-    if (route.allowedRoles && !route.allowedRoles.includes(user?.data.rol)) return false;
-    
+    // Si la ruta no requiere autenticación, es accesible para usuarios no autenticados
+    if (!route.requiresAuth) 
+      return true;
+    // Si la ruta requiere autenticación y no hay usuario, no es accesible
+    if (route.requiresAuth && !user) 
+      return false;
+    // Si la ruta tiene roles permitidos, verifica que el usuario tenga uno de esos roles
+    if (route.allowedRoles && !route.allowedRoles.includes(user?.data?.rol)) 
+      return false;
     return true;
   };
-  
   
   const getResourceOwnerId = (route) => {
     const match = matchPath(route.path, location.pathname);
@@ -97,11 +106,21 @@ function App() {
               key={route.path}
               path={route.path}
               element={
-                user && ["/login", "/registro"].includes(route.path) ? (
+                console.log({
+                path: route.path,
+                requiresAuth: route.requiresAuth,
+                user: !!user,
+                checkAccess: checkAccess(route, user),
+                location: location.pathname,
+              }) ||
+                // Si el usuario está autenticado y la ruta no requiere autenticación, redirige a "/"
+                user && !route.requiresAuth ? (
                   <Navigate to="/" replace />
-                ) : !checkAccess(route, user, { resourceOwnerId: ownerId }) ? (
+                ) : // Si el usuario no tiene acceso según checkAccess, redirige a "/" si está autenticado, o a "/login" si no
+                !checkAccess(route, user) ? (
                   <Navigate to={user ? "/" : "/login"} replace />
                 ) : (
+                  // Si todo está bien, renderiza la página con su título
                   <PageTitle title={route.title}>{route.element}</PageTitle>
                 )
               }
