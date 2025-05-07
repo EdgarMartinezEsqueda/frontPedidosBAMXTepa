f315403d50cc6efcc866d60d1ebfd458860fe3ed
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-05-06 21:34:12 -0600

    style: dark mode improved

--- src/components/selects/Select.jsx
+++ src/components/selects/Select.jsx
@@ -4,11 +4,11 @@ const Select = ({ name, value, options = [], onChange, placeholder }) => {
       name={name}
       value={value}
       onChange={onChange}
-      className="mt-1 block rounded-md border border-gray-300 px-3 py-2 focus:border-verdeLogo focus:outline-none focus:ring-2 focus:ring-verdeLogo text-gray-700 mx-auto"
+      className="mt-1 block rounded-md border border-gray-300 px-3 py-2 focus:border-verdeLogo focus:outline-none focus:ring-2 focus:ring-verdeLogoe mx-auto"
     >
-      <option value="">{placeholder}</option>
+      <option value="" className="text-gray-700 dark:text-white dark:bg-gray-800">{placeholder}</option>
       {options.map((item) => (
-        <option key={item.id} value={item.id}>
+        <option key={item.id} value={item.id} className="text-gray-700 dark:text-white dark:bg-gray-800">
           {item.nombre}
         </option>
       ))}
