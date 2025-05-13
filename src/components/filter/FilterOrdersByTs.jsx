66ce1eb25a2d64ef169deef88183aeb3a6c37d7a
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-05-12 23:24:42 -0600

    feat: create filter component for TS's orders page

---
+++ src/components/filter/FilterOrdersByTs.jsx
@@ -0,0 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import FilterDropdown from "./components/FilterDropdown";
+import DateFilter from "./components/DateFilter";
+import api from "lib/axios";
+
+const FilterWrapper = ({
+  routes,
+  setRoutes,
+  dateRange,
+  setDateRange,
+  statusOrder,
+  setStatusOrder
+}) => {
+  // Fetch para trabajadores
+  const { data: pedidosData } = useQuery({
+    queryKey: ["pedidos"],
+    queryFn: async () => {
+      const { data } = await api.get("/pedidos");
+      return data;
+    },
+  });
+
+  // Procesar datos para los filtros
+  const availableRoutes = [ ...new Set( pedidosData?.map(u => u.ruta.nombre) ) ].reverse() || [];
+  const statusOrders = ["pendiente", "finalizado"];
+  
+  return (
+    <div className="flex flex-wrap gap-4 p-4 sm:gap-8 items-center justify-center">
+      <FilterDropdown
+        title="Rutas"
+        allItems={availableRoutes}
+        selectedItems={routes}
+        onSelectionChange={setRoutes}
+        loading={!pedidosData}
+      />
+
+      <FilterDropdown
+        title="Estatus"
+        allItems={statusOrders}
+        selectedItems={statusOrder}
+        onSelectionChange={setStatusOrder}
+      />
+
+      <DateFilter onDateChange={setDateRange} />
+    </div>
+  );
+};
+
+export default FilterWrapper;
