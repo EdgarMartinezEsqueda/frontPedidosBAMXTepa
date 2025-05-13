cea9c8d33866286bdfe4ad8ade40913dd0a4aa37
EdgarMartinezEsqueda <edgar.martinez5297@alumnos.udg.mx>
2025-05-12 23:22:42 -0600

    feat: add button to download filtered orders

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ import Footer from "components/footer/Footer";
 import TableOrders from "components/tables/orders/TableOrders";
 import FilterOrders from "components/filter/FilterOrders";
 import Pagination from "components/pagination/Pagination";
+import ExportButton from "../components/buttons/ExportButton";
 
 const HomePage = () => {  
   // Estados de paginación
@@ -33,7 +34,18 @@ const HomePage = () => {
       <Navbar />
       <main className="flex-1">
 
-        <h2 className="text-center mt-4 font-bold text-2xl text-verdeLogo">Pedidos</h2>
+        <div className="container px-4 mx-auto mt-4 flex justify-between items-center">
+          <h2 className="font-bold text-2xl text-verdeLogo">Pedidos</h2>
+          <ExportButton 
+            filters={{
+              usuarios: workers,
+              rutas: routes,
+              estatusPedido: statusOrder,
+              startDate: dateRange.startDate,
+              endDate: dateRange.endDate
+            }}
+          />
+        </div>
 
         <FilterOrders 
           workers={workers}
@@ -45,6 +57,7 @@ const HomePage = () => {
           statusOrder={statusOrder}
           setStatusOrder={setStatusOrder}        
         />
+
         <div className="container px-4 mx-auto md:py-4">
           <TableOrders 
             currentPage={currentPage}
